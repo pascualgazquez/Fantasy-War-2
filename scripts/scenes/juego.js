cfce4815db97f609e8d508157dee4aac794e99a3
pascualgazquez <pascual.gazquez.compan@gmail.com>
2023-04-05 11:10:52 +0200

    Escena de juego casi terminada
    - siguiente: clase de tropas

--- scripts/scenes/juego.js
+++ scripts/scenes/juego.js
@@ -7,14 +7,20 @@ export class juego extends Phaser.Scene {
     preload() {
 
         // img
-        this.load.spritesheet('fondo', '../resources/img/fondos/fondo_inicio.png', { frameWidth: 320, frameHeight: 180 });
-        this.load.image('player1t', '../resources/img/lobby/lobby_player1.png');
-        this.load.image('player2t', '../resources/img/lobby/lobby_player2.png');
-        this.load.image('readyYes', '../resources/img/lobby/lobby_ready_yes.png');
-        this.load.image('readyNo', '../resources/img/lobby/lobby_ready_no.png');
-        this.load.image('start', '../resources/img/lobby/lobby_start.png');
+        this.load.image('fondoJ', '../resources/img/fondos/fondo_juego.png');
+      
         this.load.image('arrow', '../resources/img/lobby/lobby_arrow.png');
         this.load.image('arrow2', '../resources/img/lobby/lobby_arrow_r.png');
+
+        this.load.image('cartaMago1', '../resources/img/juego/cartaMago1.png');
+        this.load.image('cartaMago2', '../resources/img/juego/cartaMago2.png');
+        this.load.image('cartaGoblin1', '../resources/img/juego/cartaGoblin1.png');
+        this.load.image('cartaGoblin2', '../resources/img/juego/cartaGoblin2.png');
+        this.load.image('cartaGolem1', '../resources/img/juego/cartaGolem1.png');
+        this.load.image('cartaGolem2', '../resources/img/juego/cartaGolem2.png');
+
+        this.load.spritesheet('coin', '../resources/img/juego/coin.png', { frameWidth: 20, frameHeight: 20 });
+
 
         // musica
         this.load.audio('aceptar', '../resources/music/aceptar.mp3');
@@ -25,77 +31,160 @@ export class juego extends Phaser.Scene {
 
     create() {
 
+        this.arrowX = 8;
+
         // sound
         this.aceptar = this.sound.add('aceptar');
         this.rechazar = this.sound.add('rechazar');
         this.cambiar = this.sound.add('cambiar');
 
         // img
-        this.fondo = this.add.sprite(320 / 2, 180 / 2, 'fondo');
+        this.fondo = this.add.sprite(320 / 2, 180 / 2, 'fondoJ');
 
-        this.player1text = this.add.sprite(40, 30, 'player1t');
-        this.player2text = this.add.sprite(320 - 40, 30, 'player2t');
+        this.arrowP1 = this.add.sprite(this.arrowX, 100, 'arrow');
+        this.arrowP2 = this.add.sprite(320 - this.arrowX, 100, 'arrow2');
 
-        this.readyP1text = this.add.sprite(50, 120, 'readyNo');
-        this.readyP2text = this.add.sprite(320 - 50, 120, 'readyNo');
+        this.coinP1 = this.add.sprite(8, 10, 'coin');
+        this.coinP2 = this.add.sprite(320 - 8, 10, 'coin');
 
-        this.startP1text = this.add.sprite(52, 140, 'start');
-        this.startP1text = this.add.sprite(320 - 54, 140, 'start');
+        this.cartaGoblin1 = this.add.sprite(60, 165, 'cartaGoblin1').setScale(.6, .6);
+        this.cartaMago1 = this.add.sprite(84, 165, 'cartaMago1').setScale(1,1);
+        this.cartaGolem1 = this.add.sprite(108, 165, 'cartaGolem1').setScale(.6, .6);
 
-        this.arrowP1 = this.add.sprite(18, 120, 'arrow');
-        this.arrowP2 = this.add.sprite(320 - 18, 120, 'arrow2');
+        this.cartaGolem2 = this.add.sprite(320 - 60, 165, 'cartaGolem2').setScale(.6, .6);
+        this.cartaMago2 = this.add.sprite(320 - 84, 165, 'cartaMago2').setScale(1, 1);
+        this.cartaGoblin2 = this.add.sprite(320 - 108, 165, 'cartaGoblin2').setScale(.6, .6);
 
         // animations
-        this.createAnim('fondoAnim', 'fondo', 1);
-        this.fondo.anims.play('fondoAnim');
+        this.createAnim('coinAnim', 'coin', 3);
+        this.coinP1.anims.play('coinAnim');
+        this.coinP2.anims.play('coinAnim');
+
+        // texto
+        this.textGold1 = this.add.text(16, 4, '10', { font: "12px 'PS2P'", color: '#fff200' });
+        this.textGold2 = this.add.text(320 - 40, 4, '10', { font: "12px 'PS2P'", color: '#fff200' });
+        this.goldP1 = 10;
+        this.goldP2 = 10;
 
         //controles
 
         //P1
         this.keyW = this.input.keyboard.addKey('W');
         this.keyS = this.input.keyboard.addKey('S');
+        this.keyA = this.input.keyboard.addKey('A');
+        this.keyD = this.input.keyboard.addKey('D');
         this.keySpace = this.input.keyboard.addKey('SPACE');
 
         //P2
         this.keyUp = this.input.keyboard.addKey('UP');
         this.keyDown = this.input.keyboard.addKey('DOWN');
+        this.keyLeft = this.input.keyboard.addKey('LEFT');
+        this.keyRight = this.input.keyboard.addKey('RIGHT');
         this.keyEnter = this.input.keyboard.addKey('ENTER');
 
-        this.botonP1 = 1;
-        this.botonP2 = 1;
-        this.readyP1 = 0;
-        this.readyP2 = 0;
+        this.caminoP1 = 2;
+        this.caminoP2 = 2;
+
+        this.tropaP1 = 2;
+        this.tropaP2 = 2;
 
     }
 
     update() {
 
+        // GOLD
+        if (this.goldP1 < 99) { this.goldP1 += 1;}
+        if (this.goldP2 < 99) { this.goldP2 += 1; }
+
+        this.textGold1.setText(this.goldP1);
+        this.textGold2.setText(this.goldP2);
+
         // P1 ///////////////////////////////////
 
-        // TECLA ARRIBA P1
         if (Phaser.Input.Keyboard.JustDown(this.keyW)) {
 
-            // READY
-            if (this.botonP1 == 2) {
+            // camino 3
+            if (this.caminoP1 == 3) {
                 this.cambiar.play();
-                this.botonP1 = 1;
+                this.caminoP1 = 2;
 
                 this.arrowP1.destroy()
-                this.arrowP1 = this.add.image(18, 120, 'arrow');
+                this.arrowP1 = this.add.image(this.arrowX, 100, 'arrow');
+            }
+
+            // camino 2
+            else if (this.caminoP1 == 2) {
+                this.cambiar.play();
+                this.caminoP1 = 1;
+
+                this.arrowP1.destroy()
+                this.arrowP1 = this.add.image(this.arrowX, 45, 'arrow');
             }
 
         }
 
-        // TECLA ABAJO P1
         if (Phaser.Input.Keyboard.JustDown(this.keyS)) {
 
-            // START
-            if ((this.botonP1) == 1) {
+            // camino 3
+            if (this.caminoP1 == 1) {
                 this.cambiar.play();
-                this.botonP1 = 2;
+                this.caminoP1 = 2;
 
                 this.arrowP1.destroy()
-                this.arrowP1 = this.add.image(18, 140, 'arrow');
+                this.arrowP1 = this.add.image(this.arrowX, 100, 'arrow');
+            }
+
+            // camino 2
+            else if (this.caminoP1 == 2) {
+                this.cambiar.play();
+                this.caminoP1 = 3;
+
+                this.arrowP1.destroy()
+                this.arrowP1 = this.add.image(this.arrowX, 155, 'arrow');
+            }
+
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(this.keyD)) {
+
+            // goblin
+            if (this.tropaP1 == 1) {
+                this.cambiar.play();
+                this.tropaP1 = 2;
+
+                this.cartaGoblin1.setScale(.6, .6);
+                this.cartaMago1.setScale(1, 1);
+            }
+
+            // tropa 2
+            else if (this.tropaP1 == 2) {
+                this.cambiar.play();
+                this.tropaP1 = 3;
+
+                this.cartaMago1.setScale(.6, .6);
+                this.cartaGolem1.setScale(1, 1); 
+            }
+
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(this.keyA)) {
+
+            // golem
+            if (this.tropaP1 == 3) {
+                this.cambiar.play();
+                this.tropaP1 = 2;
+
+                this.cartaGolem1.setScale(.6, .6);
+                this.cartaMago1.setScale(1, 1);
+            }
+
+            // tropa 2
+            else if (this.tropaP1 == 2) {
+                this.cambiar.play();
+                this.tropaP1 = 1;
+
+                this.cartaMago1.setScale(.6, .6);
+                this.cartaGoblin1.setScale(1, 1);
             }
 
         }
@@ -103,118 +192,103 @@ export class juego extends Phaser.Scene {
         // TECLA ACEPTAR P1
         if (Phaser.Input.Keyboard.JustDown(this.keySpace)) {
 
-            // READY
-            if (this.botonP1 == 1) {
-
-                // sonido
-                this.aceptar.play();
-
-                // funcionalidad
-                if (this.readyP1 == 0) {
-
-                    this.readyP1text.destroy();
-                    this.readyP1text = this.add.sprite(50, 120, 'readyYes');
-                    this.readyP1 = 1;
-
-                }
-                else if (this.readyP1 == 1) {
-
-                    this.readyP1text.destroy();
-                    this.readyP1text = this.add.sprite(50, 120, 'readyNo');
-                    this.readyP1 = 0;
-                }
-
-            }
-
-            // START!
-            if (this.botonP1 == 2) {
-
-                // Preparados
-                if ((this.readyP1 == 1) && (this.readyP2 == 1)) {
-
-                    // sonido
-                    this.aceptar.play();
-                    // funcionalidad
-                    this.scene.start('inicio')
-
-                }
-                else { this.rechazar.play(); }
-
-            }
 
         }
 
         // P2 ///////////////////////////////////
 
-        // TECLA ARRIBA P2
         if (Phaser.Input.Keyboard.JustDown(this.keyUp)) {
 
-            // READY
-            if (this.botonP2 == 2) {
+            // camino 3
+            if (this.caminoP2 == 3) {
                 this.cambiar.play();
-                this.botonP2 = 1;
+                this.caminoP2 = 2;
 
                 this.arrowP2.destroy()
-                this.arrowP2 = this.add.image(320 - 18, 120, 'arrow2');
+                this.arrowP2 = this.add.image(320 - this.arrowX, 100, 'arrow2');
+            }
+
+            // camino 2
+            else if (this.caminoP2 == 2) {
+                this.cambiar.play();
+                this.caminoP2 = 1;
+
+                this.arrowP2.destroy()
+                this.arrowP2 = this.add.image(320 - this.arrowX, 45, 'arrow2');
             }
 
         }
 
-        // TECLA ABAJO P2
         if (Phaser.Input.Keyboard.JustDown(this.keyDown)) {
 
-            // START
-            if ((this.botonP2) == 1) {
+            // camino 3
+            if (this.caminoP2 == 1) {
                 this.cambiar.play();
-                this.botonP2 = 2;
+                this.caminoP2 = 2;
 
                 this.arrowP2.destroy()
-                this.arrowP2 = this.add.image(320 - 18, 140, 'arrow2');
+                this.arrowP2 = this.add.image(320 - this.arrowX, 100, 'arrow2');
+            }
+
+            // camino 2
+            else if (this.caminoP2 == 2) {
+                this.cambiar.play();
+                this.caminoP2 = 3;
+
+                this.arrowP2.destroy()
+                this.arrowP2 = this.add.image(320 - this.arrowX, 155, 'arrow2');
             }
 
         }
 
-        // TECLA ACEPTAR P2
+        if (Phaser.Input.Keyboard.JustDown(this.keyRight)) {
+
+            // goblin
+            if (this.tropaP2 == 1) {
+                this.cambiar.play();
+                this.tropaP2 = 2;
+
+                this.cartaGoblin2.setScale(.6, .6);
+                this.cartaMago2.setScale(1, 1);
+            }
+
+            // mago
+            else if (this.tropaP2 == 2) {
+                this.cambiar.play();
+                this.tropaP2 = 3;
+
+                this.cartaMago2.setScale(.6, .6);
+                this.cartaGolem2.setScale(1, 1);
+            }
+
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(this.keyLeft)) {
+
+            // golem
+            if (this.tropaP2 == 3) {
+                this.cambiar.play();
+                this.tropaP2 = 2;
+
+                this.cartaGolem2.setScale(.6, .6);
+                this.cartaMago2.setScale(1, 1);
+            }
+
+            // tropa 2
+            else if (this.tropaP2 == 2) {
+                this.cambiar.play();
+                this.tropaP2 = 1;
+
+                this.cartaMago2.setScale(.6, .6);
+                this.cartaGoblin2.setScale(1, 1);
+            }
+
+        }
+
+
+        // TECLA ACEPTAR P1
         if (Phaser.Input.Keyboard.JustDown(this.keyEnter)) {
 
-            // READY
-            if (this.botonP2 == 1) {
-
-                // sonido
-                this.aceptar.play();
-
-                // funcionalidad
-                if (this.readyP2 == 0) {
-
-                    this.readyP2text.destroy();
-                    this.readyP2text = this.add.sprite(320 - 50, 120, 'readyYes');
-                    this.readyP2 = 1;
-
-                }
-                else if (this.readyP2 == 1) {
-
-                    this.readyP2text.destroy();
-                    this.readyP2text = this.add.sprite(320 - 50, 120, 'readyNo');
-                    this.readyP2 = 0;
-                }
-
-            }
-
-            // START!
-            if (this.botonP2 == 2) {
-
-                // Preparados
-                if ((this.readyP2 == 1) && (this.readyP2 == 1)) {
-
-                    // sonido
-                    this.aceptar.play();
-                    // funcionalidad
-                    this.scene.start('inicio')
-
-                }
-                else { this.rechazar.play(); }
-
-            }
 
         }
 
